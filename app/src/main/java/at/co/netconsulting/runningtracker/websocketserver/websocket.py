02e614fe869afb0a4b917827efd87ebbf897e080
bernd-roth <berndroth0@gmail.com>
2024-09-21 22:43:50 +0200

    index.html: info window will be opened automatically; more information in info window will be displayed immediately websocket.py: logging mechanism implemented

--- app/src/main/java/at/co/netconsulting/runningtracker/websocketserver/websocket.py
+++ app/src/main/java/at/co/netconsulting/runningtracker/websocketserver/websocket.py
@@ -4,6 +4,10 @@ import asyncio
 import datetime
 import websockets
 import json
+import logging
+
+# Configure logging
+logging.basicConfig(filename='/home/bernd/websocket.log', level=logging.INFO)
 
 # Set to store connected clients
 connected_clients = set()
@@ -22,6 +26,7 @@ async def handle_client(websocket, path):
     connected_clients.add(websocket)
     now = datetime.datetime.now()
     print(f"New client connected at {now.strftime('%d-%m-%Y %H:%M:%S')}")
+    logging.info(f"New client connected at {now.strftime('%d-%m-%Y %H:%M:%S')}")
 
     # On connection, send the last 24 hours' messages to the new client
     cleanup_old_messages()  # Clean up old messages before sending
@@ -34,12 +39,14 @@ async def handle_client(websocket, path):
             now = datetime.datetime.now()
             print(f"Received message at {now.strftime('%d-%m-%Y %H:%M:%S')}")
             print(f"Received message is: {message}")
+            logging.info(f"Received message at {now.strftime('%d-%m-%Y %H:%M:%S')}")
+            logging.info(f"Received message is: {message}")
 
             # Parse the received message as JSON
             message_data = json.loads(message)
 
             # Check if the message contains a sessionId, lat, lng (valid data)
-            if all(key in message_data for key in ("sessionId", "latitude", "longitude")):
+            if all(key in message_data for key in ("person", "sessionId", "latitude", "longitude", "distance", "currentSpeed")):
                 # Save the message with the timestamp
                 message_history.append((message_data, now))
 
@@ -54,16 +61,19 @@ async def handle_client(websocket, path):
                 cleanup_old_messages()
             else:
                 print("Invalid message format. Missing required fields.")
+                logging.info("Invalid message format. Missing required fields.")
     except websockets.exceptions.ConnectionClosed as e:
         print(f"Client disconnected: {websocket.remote_address}")
+        logging.info(f"Client disconnected: {websocket.remote_address}")
 
     finally:
         # Unregister the client
         connected_clients.remove(websocket)
 
 async def main():
-    server = await websockets.serve(handle_client, "0.0.0.0", SERVER_PORT)
+    server = await websockets.serve(handle_client, "0.0.0.0", MY_PORT)
     print("WebSocket server is running")
+    logging.info("WebSocket server is running")
     await asyncio.Future()  # Run forever
 
 if __name__ == "__main__":
